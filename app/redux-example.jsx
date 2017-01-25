5f29034b52eed34c4d89e69849355760bb24e64a
Hector Rojas <hrpxa270@hotmail.com>
2017-01-24 23:30:16 -0600

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,6 +1,8 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
+
 
 // Name reducer and action generators
 // -----------------
@@ -93,10 +95,55 @@ var removeMovie = (id) => {
 };
 
 
+// Map reducer and action generators
+// -----------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url: undefined
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            };
+        default:
+            return state;
+    }
+};
+
+var startLocationFetch = () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+    };
+};
+
+var completeLocationFetch = (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    };
+};
+
+var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+
+    axios.get('http://ipinfo.io').then(function (res) {
+        var loc = res.data.loc;
+        var baseUrl = 'http://maps.google.com?q='
+
+        store.dispatch(completeLocationFetch(baseUrl + loc));
+    });
+};
+
+
 var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
 });
 
 
@@ -104,14 +151,18 @@ var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+
 // Subscribe to changes
 var unsubscribe = store.subscribe(() => {
     var state = store.getState();
 
-    console.log('Name is', state.name);
-    document.getElementById('app').innerHTML = state.name;
-
     console.log('New state', store.getState());
+
+    if (state.map.isFetching) {
+        document.getElementById('app').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>';
+    }
 });
 // unsubscribe();
 
@@ -119,14 +170,15 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
+fetchLocation();
 
-store.dispatch(changeName('Hector'));
+store.dispatch(changeName('Andrew'));
 
 store.dispatch(addHobby('Running'));
 store.dispatch(addHobby('Walking'));
 store.dispatch(removeHobby(2));
 
-store.dispatch(changeName('Raul'));
+store.dispatch(changeName('Emily'));
 
 store.dispatch(addMovie('Mad Max', 'Action'));
 store.dispatch(addMovie('Start Wars', 'Action'));
