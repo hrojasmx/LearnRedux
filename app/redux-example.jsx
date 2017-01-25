96ea2191e4c8aa52088a8dfb5a0640db4adf8023
Hector Rojas <hrpxa270@hotmail.com>
2017-01-24 21:17:30 -0600

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,9 +5,37 @@ console.log('Starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
     // state = state || {name: 'Anonymous'};
 
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            };
+        default:
+            return state;
+    }
+
+    console.log('New action-> ', action)
     return state;
 };
+
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+/*
+var action = {
+    type: 'CHANGE_NAME',
+    name: 'Hector'
+
+}
+store.dispatch(action);
+*/
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Hector'
+});
+
+console.log('Name should be Hector', store.getState());
