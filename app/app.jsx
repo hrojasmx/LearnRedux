3d4174678a40db8b6e19f9e782fc2a72ee85c98c
Hector Rojas <hrpxa270@hotmail.com>
2017-02-05 10:12:25 -0600

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,20 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
+
 // Load foundation
 // require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
@@ -17,5 +31,11 @@ ReactDOM.render(
 );
 */
 
+/*
 require('./redux-example.jsx');
 //require('./redux-todo-example.jsx');
+*/
+ReactDOM.render(
+    <TodoApp/>,
+    document.getElementById('app')
+);
--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,6 +1,8 @@
 var redux = require('redux');
 var thunk = require('redux-thunk').default;
-var {nameReducer, hobbiesReducer, moviesReducer, mapReducer} = require('./../reducers/index');
+var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+/*
+var {nameReducer, hobbiesReducer, moviesReducer, mapReducer,} = require('./../reducers/index');
 
 export var configure = () => {
   var reducer = redux.combineReducers({
@@ -13,6 +15,17 @@ export var configure = () => {
   var store = redux.createStore(reducer, redux.compose(
     redux.applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+*/
+export var configure = () => {
+  var reducer = redux.combineReducers({
+    searchText: searchTextReducer,
+    showCompleted: showCompletedReducer,
+    todos: todosReducer
+  });
+
+  var store = redux.createStore(reducer, redux.compose(
+      window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
 
   return store;
